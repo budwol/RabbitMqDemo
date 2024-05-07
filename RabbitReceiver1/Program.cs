e9213815aa5690a7493a61f0c5c790d1fecabeb2
budwol <wolf.budgenhagen@gmail.com>
2024-05-07 09:59:46 +0200

    :tada: initial commit

---
+++ RabbitReceiver1/Program.cs
@@ -0,0 +1,55 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+#region Setup RabbitMQ
+
+ConnectionFactory factory = new()
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672"), // connection string | ToDo --> json
+    ClientProvidedName = "Rabbit Receiver2 App"
+};
+
+var cnn = factory.CreateConnection();
+var channel = cnn.CreateModel();
+
+const string exchangeName = "DemoExchange";
+const string routingKey = "demo-routing-key";
+const string queName = "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queName,false, false, false,null);
+channel.QueueBind(queName,exchangeName,routingKey);
+
+// RECEIVER
+channel.BasicQos(0,1,false);
+
+#endregion
+
+#region Receive Message
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, args) =>
+{
+    // simulate business
+    Task.Delay(TimeSpan.FromSeconds(5)).Wait();
+    
+    var body = args.Body.ToArray();
+    var msg = Encoding.UTF8.GetString(body);
+    
+    Console.WriteLine($"Message received: {msg}");
+    
+    // notify the server that the operation is completed successfully
+    channel.BasicAck(args.DeliveryTag,false);
+};
+
+var consumerTag = channel.BasicConsume(queName, false, consumer);
+
+Console.ReadLine();
+
+channel.BasicCancel(consumerTag);
+
+channel.Close();
+cnn.Close();
+
+#endregion
---
+++ RabbitSender/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+#region Setup RabbitMQ
+
+ConnectionFactory factory = new()
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672"), // connection string | ToDo --> json
+    ClientProvidedName = "Rabbit Sender App"
+};
+
+var cnn = factory.CreateConnection();
+var channel = cnn.CreateModel();
+
+const string exchangeName = "DemoExchange";
+const string routingKey = "demo-routing-key";
+const string queName = "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queName, false, false, false, null);
+channel.QueueBind(queName, exchangeName, routingKey);
+
+#endregion
+
+#region SendMessage
+
+for (var i = 0; i < 100; i++)
+{
+    var messageBodyBytes = Encoding.UTF8.GetBytes($"Sending Message {i}");
+    channel.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
+    Task.Delay(TimeSpan.FromSeconds(1)).Wait();
+}
+
+channel.Close();
+cnn.Close();
+
+#endregion
